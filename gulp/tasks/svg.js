cd2943ef5f4ab62f7be628b8cb9283eb5891ec79
Mikhail <mexcaptain@gmail.com>
2017-12-20 06:26:58 +0300

    Text examples, button and label

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -3,25 +3,25 @@ module.exports = function() {
         return $.gulp.src('./dev/static/img/svg/*.svg')
             .pipe($.gp.svgmin({
                 js2svg: {
-                    pretty: true
+                    pretty: false
                 }
             }))
             .pipe($.gp.cheerio({
-                run: function($) {
-                    $('[fill]').removeAttr('fill');
-                    $('[stroke]').removeAttr('stroke');
-                    $('[style]').removeAttr('style');
-                },
+                // run: function($) {
+                //     $('[fill]').removeAttr('fill');
+                //     $('[stroke]').removeAttr('stroke');
+                //     $('[style]').removeAttr('style');
+                // },
                 parserOptions: { xmlMode: true }
             }))
             .pipe($.gp.replace('&gt;', '>'))
-            .pipe($.gp.svgSprite({
-                mode: {
-                    symbol: {
-                        sprite: "sprite.svg"
-                    }
-                }
-            }))
+            // .pipe($.gp.svgSprite({
+            //     mode: {
+            //         symbol: {
+            //             sprite: "sprite.svg"
+            //         }
+            //     }
+            // }))
             .pipe($.gulp.dest('./build/static/img/svg/'));
     });
 };
